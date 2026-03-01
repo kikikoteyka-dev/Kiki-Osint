a1cbc74b0229d99049a0534fc7c4fa34195c1788
kikikoteyka-dev <kiki.koteyka@gmail.com>
2026-03-01 12:08:58 +0300

    chore: remove unused files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-VK_TOKEN = os.getenv("VK_TOKEN", "")
-TG_API_ID = int(os.getenv("TG_API_ID", "0"))
-TG_API_HASH = os.getenv("TG_API_HASH", "")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
